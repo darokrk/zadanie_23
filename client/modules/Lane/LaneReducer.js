5b878f1cc55113b29672349744a54f9fcb83a00e
Dariusz Rosiek <daro.krk@interia.pl>
2018-04-27 21:38:52 +0200

    Update LaneReducer

--- client/modules/Lane/LaneReducer.js
+++ client/modules/Lane/LaneReducer.js
@@ -4,6 +4,10 @@ import {
   UPDATE_LANE,
   DELETE_LANE
 } from './LaneActions';
+import {
+  CREATE_NOTE,
+  DELETE_NOTE
+} from '../Note/NoteActions';
 
 const initialState = [];
 
@@ -11,12 +15,33 @@ export default function lanes(state = initialState, action) {
   switch (action.type) {
     case CREATE_LANE:
       return [...state, action.lane];
+
     case UPDATE_LANE:
       return state.map(lane => {
         return lane.id === action.id ? { ...lane, ...action.lane } : lane;
       });
+
     case DELETE_LANE:
       return state.filter(lane => lane.id !== action.laneId);
+
+    case CREATE_NOTE:
+      return state.map(lane => {
+        if (lane.id === action.laneId) {
+          const notes = [...lane.notes, action.note.id];
+          return { ...lane, notes };
+        }
+        return lane;
+      });
+
+    case DELETE_NOTE:
+      return state.map(lane => {
+        if (lane.id === action.laneId) {
+          const notes = [...lane.notes];
+          return notes.filter((note) => note.id !== action.noteId);
+        }
+        return lane;
+      });
+
     default:
       return state;
     }
